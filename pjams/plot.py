bda63a446c5c2e9f1cdc0582e41b0df92c88969a
Emiko <emikogardiner@berkeley.edu>
2023-06-18 01:11:52 -0700

    All ionfrac images complete with and without contours, in figures/ionfrac_maps.

--- pjams/plot.py
+++ pjams/plot.py
@@ -11,10 +11,17 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 
+from PIL import Image, ImageDraw, ImageFont
+from IPython.display import Image as DisplayImage
+
+
 FIGSIZE = 6
 FONTSIZE = 13
 GOLDEN_RATIO = (np.sqrt(5) - 1) / 2
 COLORS = np.array(['#ff3333', '#ff8c1a', '#bfbd2e',  '#5cd699', '#51a6fb', '#a65eed'])
+
+MYFONT = ImageFont.truetype('/System/Library/Fonts/Supplemental/Times New Roman.ttf', 65)
+SMALLFONT = ImageFont.truetype('/System/Library/Fonts/Supplemental/Times New Roman.ttf', 55)
 
 
 mpl.style.use('default')   # avoid dark backgrounds from dark theme vscode
@@ -205,3 +212,106 @@ def figax(figsize=[7, 5], ncols=1, nrows=1, sharex=False, sharey=False, squeeze=
             axes = axes[()]
 
     return fig, axes
+
+
+
+#########################################################
+#### PIL Images
+#########################################################
+
+def pil_image(imgfiles, debug=True, head=0):
+    """ 
+    Build a PIL image using the imgs in imgfiles
+    First row should include y-axes and bottom row should include x-axes.
+    """
+    nrows = len(imgfiles)
+    ncols = len(imgfiles[0])
+    if debug: print(f"{nrows=}, {ncols=}")
+
+    # get side lengths
+    corner_img = Image.open(imgfiles[nrows-1, 0])
+    middle_img = Image.open(imgfiles[0,1])
+    side = middle_img.width
+    left = corner_img.width - middle_img.width
+    bott = corner_img.height - middle_img.height
+    if debug: print(f"{corner_img.size=}, {middle_img.size=}")
+    if debug: print(f"{side=}, {left=}, {bott=}, {head=}")
+
+    # make allimage
+    allimage=Image.new("RGBA", (ncols*side+left, nrows*side+bott+head))
+
+    # paste mini images
+    yy = head
+    for rr in range(nrows):
+        xx = 0 
+        for cc in range(ncols):
+            if debug: print(f"{rr=}, {cc=}, {xx=}, {yy=}")
+            img = Image.open(imgfiles[rr,cc])
+            allimage.paste(img, ((xx, yy)))
+            if cc==0: xx += left+side
+            else: xx += side
+        yy += side
+    
+    details = {
+        'nrows':nrows, 'ncols':ncols, 'side':side, 'left':left, 'bott':bott, 'head':head
+    }
+    return allimage, details
+
+
+
+#########################################################
+##### Ionization Fraction PIL Image Functions
+#########################################################
+
+def pil_ionfrac_header(allimage, side, leftax, header): 
+   # add header
+    draw = ImageDraw.Draw(allimage)
+    myFont = MYFONT 
+    smallFont = SMALLFONT
+
+    mname = "Mass-Weighted"
+    # mw, mh = draw.textsize(mname)
+    vname = "Volume-Weighted"
+    # vw, vh = draw.textsize(mname)
+    ename = "Emission-Weighted"
+    # ew, eh = draw.textsize(mname)
+
+    t1 = leftax+(1.5*side)
+    t2 = t1+3*side
+    t3 = t2+3*side
+
+    draw.text((t1,0), mname, font=myFont, fill=(0,0,0), anchor='mt')
+    draw.text((t2,0), vname, font=myFont, fill=(0,0,0), anchor='mt')
+    draw.text((t3,0), ename, font=myFont, fill=(0,0,0), anchor='mt')
+
+    draw.text((leftax+60,header-60), "1 km/s", font=smallFont, fill=(26,38,134))
+    draw.text((leftax+40+side,header-60), "10 km/s", font=smallFont, fill=(26,38,134))
+    draw.text((leftax+30+2*side,header-60), "100 km/s", font=smallFont, fill=(26,38,134))
+    draw.text((leftax+60+3*side,header-60), "1 km/s", font=smallFont, fill=(26,38,134))
+    draw.text((leftax+40+4*side,header-60), "10 km/s", font=smallFont, fill=(26,38,134))
+    draw.text((leftax+30+5*side,header-60), "100 km/s", font=smallFont, fill=(26,38,134))
+    draw.text((leftax+60+6*side,header-60), "1 km/s", font=smallFont, fill=(26,38,134))
+    draw.text((leftax+40+7*side,header-60), "10 km/s", font=smallFont, fill=(26,38,134))
+    draw.text((leftax+30+8*side,header-60), "100 km/s", font=smallFont, fill=(26,38,134))
+    return allimage
+
+
+def pil_ionfrac_section_breaks(allimage, side, leftax, header):
+    # add section breaks
+    line1 = [(leftax+3*side,0), (leftax+3*side, header)]
+    line2 = [(leftax+6*side,0), (leftax+6*side, header)]
+    draw = ImageDraw.Draw(allimage)
+    draw.line(line1,fill=(0,0,0),width=5)
+    draw.line(line2,fill=(0,0,0),width=5)
+    return allimage
+
+
+def ionfrac_pil_image(imgfiles, debug=False):
+    allimage, details = pil_image(imgfiles, head=150, debug=debug)
+    side = details['side']
+    left = details['left']
+    head = details['head']
+    allimage = pil_ionfrac_header(allimage, side, left, head)
+    allimage = pil_ionfrac_section_breaks(allimage, side, left, head)
+    # allimage = add_mass_labels(allimage, side, left, head, labels, debug=debug)
+    return allimage
